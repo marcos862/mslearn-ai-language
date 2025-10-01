1ab26199c1e57eb90e2c117a39789ebee3b71fe1
Marcos Bolanos <marcos.ibolanos@gmail.com>
2025-10-01 17:15:41 -0600

    Update qna-app.py

--- Labfiles/02-qna/Python/qna-app/qna-app.py
+++ Labfiles/02-qna/Python/qna-app/qna-app.py
@@ -1,7 +1,10 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from traceback import format_exc
 
-# Import namespaces
+# import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.questionanswering import QuestionAnsweringClient
 
 
 def main():
@@ -14,14 +17,25 @@ def main():
         ai_deployment_name = os.getenv('QA_DEPLOYMENT_NAME')
 
         # Create client using endpoint and key
-
+        credential = AzureKeyCredential(ai_key)
+        ai_client = QuestionAnsweringClient(endpoint=ai_endpoint, credential=credential)
 
         # Submit a question and display the answer
+        user_question = ''
+        while True:
+            user_question = input('\nQuestion:\n')
+            if user_question.lower() == "quit":                
+                break
+            response = ai_client.get_answers(question=user_question,
+                                             project_name=ai_project_name,
+                                             deployment_name=ai_deployment_name)
+            for candidate in response.answers:
+                print(candidate.answer)
+                print("Confidence: {}".format(candidate.confidence))
+                print("Source: {}".format(candidate.source))
 
-
-
-    except Exception as ex:
-        print(ex)
+    except Exception:
+        print(format_exc())
 
 
 if __name__ == "__main__":
