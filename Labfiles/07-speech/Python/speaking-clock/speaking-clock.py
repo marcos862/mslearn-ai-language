0dcb963d93af915bdedab4ee02b981dcf9dc4528
Marcos Bolanos <marcos.ibolanos@gmail.com>
2025-10-02 13:24:12 -0600

    Update speaking-clock.py

--- Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
+++ Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
@@ -60,6 +60,30 @@ def TranscribeCommand():
     return command
 
 
+def TranscribeCommand_Mic():
+    command = ''
+
+    # Configure speech recognition
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+    print('Speak now...')
+
+    # Process speech input
+    speech = speech_recognizer.recognize_once_async().get()
+    if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
+        command = speech.text
+        print(command)
+    else:
+        print(speech.reason)
+        if speech.reason == speech_sdk.ResultReason.Canceled:
+            cancellation = speech.cancellation_details
+            print(cancellation.reason)
+            print(cancellation.error_details)
+
+    # Return the command
+    return command
+
+
 def TellTime():
     now = datetime.now()
     response_text = 'The time is {}:{:02d}'.format(now.hour,now.minute)
@@ -85,6 +109,26 @@ def TellTime():
     </speak>
     """.format(response_text)
     speak = speech_synthesizer.speak_ssml_async(responseSsml).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
+    else:
+        print("Spoken output saved in " + output_file)
+    
+    # Print the response
+    print(response_text)
+
+
+def TellTime_Speaker():
+    now = datetime.now()
+    response_text = 'The time is {}:{:02d}'.format(now.hour, now.minute)
+
+    # Configure speech synthesis
+    speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
+    audio_config = speech_sdk.audio.AudioConfig(use_default_speaker=True)
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config, audio_config)
+
+    # Synthesize spoken output
+    speak = speech_synthesizer.speak_text_async(response_text).get()
     if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
         print(speak.reason)
     else:
